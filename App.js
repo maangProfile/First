f0e6c4939635c274f33c0fc5593f9f514f86bc3d
maangProfile <arunkumar.valluru@maangtechnologies.com>
2023-08-30 23:44:36 +0530

    first branch

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import {View,Text,FlatList} from 'react-native';
+import { contacts } from "./config/data";
+import colors from "./config/colors";
+
+class Contacts extends Component{
+  render(){
+    return(
+      <FlatList style={{backgroundColor:colors.background}} data={contacts} renderItem={({item})=>(<View><Text>{item.email}</Text></View>)}
+         keyExtractor={(item)=>item.email}
+        />
+    )
+  }
+}
+export default Contacts;
