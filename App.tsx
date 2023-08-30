72f61c6d75315ab00a3841eb128bc082b87d02b6
nagakirann <r180578@rguktrkv.ac.in>
2023-08-30 18:49:23 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text,View } from "react-native";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const App =()=>{
+  return (
+    <View>
+      <Text style={{fontSize:30,color:'red'}}>Hello World This the day i want to go</Text>
+    </View>
+  );
+}
+export default App;
