accf2ef3059e1af3df1ec913f96597b26ee4c156
maangProfile <arunkumar.valluru@maangtechnologies.com>
2023-09-04 23:25:19 +0530

    first

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {ScrollView, Text, View } from 'react-native';
+import Contacts from "./src/Screens/Contacts/Contacts.js";
+const App = () => {
+  return (
+    <ScrollView >
+        <Contacts />
+    </ScrollView>
+  );
+}
+
+
+export default App;
---
+++ src/Screens/Contacts/Contacts.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from "react";
+import { FlatList, Text, View } from 'react-native';
+import { contacts } from "../../Config/Data";
+import Colors from "../../Config/Colors";
+import ListItem from '../../Components/List/ListItem';
+
+
+
+
+class Contacts extends Component {
+
+  handleRowPress = (item) => {
+   return null;
+  }
+render(){
+  return (
+    
+    <FlatList
+      style={{flex:1, backgroundColor: Colors.background }}
+      data={contacts}
+      renderItem={({ item }) =>
+       <ListItem contact={item} onPress={()=>this.handleRowPress(item)}/>}
+      keyExtractor={(item) =>  item.email}
+    />
+  );
+}
+
+
+}
+  
+export default Contacts;
